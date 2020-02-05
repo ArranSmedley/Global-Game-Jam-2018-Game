0ec61566038ba9c8d13f3575aa800109fef5740a
ArranSmedley <arransmedleywork@gmail.com>
2020-02-05 19:40:55 +0000

    commit

---
+++ Programming Code/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float increment;
+    public float maxY;
+    public float minY;
+
+    private Vector2 targetPos;
+
+    public int health;
+
+    public GameObject moveEffect;
+    public Animator camAnim;
+    public Text healthDisplay;
+
+    public GameObject spawner;
+    public GameObject restartDisplay;
+
+
+    public void MoveUp()
+    {
+        camAnim.SetTrigger("shake");
+        Instantiate(moveEffect, transform.position, Quaternion.identity);
+        targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+    }
+
+    public void MoveDown()
+    {
+        camAnim.SetTrigger("shake");
+        Instantiate(moveEffect, transform.position, Quaternion.identity);
+        targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+    }
+
+    private void Update()
+    {
+
+        if (health <= 0) {
+            spawner.SetActive(false);
+            restartDisplay.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        healthDisplay.text = health.ToString();
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY)
+        {
+
+            camAnim.SetTrigger("shake");
+
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY)
+        {
+
+            camAnim.SetTrigger("shake");
+
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+
+        }
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Global-Game-Jam-2018-Game
 This game was created within the global game jam 2018 in the theme of Transmission. This game i only a prototype and was created in 48 hours by myself. This was also my first ever experience using Unity.
+
+To run the game simply download the files within the repository and go into the full game folder and run the .exe file.
+
+Disclaimer: DO NOT DELETE ANY OF THE ASSET FOLDERS WITHIN THE FULL GAME FOLDER OTHERWISE THE GAME WONT WORK.
